20d35c8ad13ac15d2b91b242b3d99aaec640c5ff
Henrique Montenegro <montenegrohenrique@gmail.com>
2021-06-17 16:38:43 -0300

    05.03 - Serializando JSON no objeto

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart';
 import 'package:http_interceptor/http_interceptor.dart';
 import 'interceptors/logging_interceptor.dart';
 
-final String baseUrl = 'https://4c6669b08467.ngrok.io/';
+final String baseUrl = 'https://24a5e8b4c301.ngrok.io/';
 
 Client client = HttpClientWithInterceptor.build(
   interceptors: [
--- lib/http/webclients/transaction_webclient.dart
+++ lib/http/webclients/transaction_webclient.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:bytebank/http/webclient.dart';
-import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/models/transaction.dart';
 import 'package:http/http.dart';
 
@@ -17,9 +16,7 @@ class TransactionWebClient {
   }
 
   Future<Transaction> save(Transaction transaction) async {
-    Map<String, dynamic> transactionMap = _toMap(transaction);
-
-    final String transactionJson = jsonEncode(transactionMap);
+    final String transactionJson = jsonEncode(transaction.toJson());
 
     final Response response = await client.post('${baseUrl}transactions',
         headers: {
@@ -38,43 +35,13 @@ class TransactionWebClient {
     // final List<Transaction> transactions = List(); // deprecated
 
     for (Map<String, dynamic> transactionJson in decodedJson) {
-      final Map<String, dynamic> contactJson = transactionJson['contact'];
-
-      final Transaction transaction = Transaction(
-        transactionJson['value'],
-        Contact(
-          0,
-          contactJson['name'],
-          contactJson['accountNumber'],
-        ),
-      );
-
-      transactions.add(transaction);
+      transactions.add(Transaction.fromJson(transactionJson));
     }
     return transactions;
   }
 
   Transaction _toTransaction(Response response) {
     Map<String, dynamic> json = jsonDecode(response.body);
-    final Map<String, dynamic> contactJson = json['contact'];
-    return Transaction(
-      json['value'],
-      Contact(
-        0,
-        contactJson['name'],
-        contactJson['accountNumber'],
-      ),
-    );
-  }
-
-  Map<String, dynamic> _toMap(Transaction transaction) {
-    final Map<String, dynamic> transactionMap = {
-      'value': transaction.value,
-      'contact': {
-        'name': transaction.contact.name,
-        'accountNumber': transaction.contact.accountNumber,
-      }
-    };
-    return transactionMap;
+    return Transaction.fromJson(json);
   }
 }
